569baae78120eb993b4eb9ef3dbc2d5fa7cd2ca4
DandyChan <dandychan@katcloud.cn>
2022-02-08 18:27:47 +0800

    update some interesting things

--- assets/js/baseUrl.js
+++ assets/js/baseUrl.js
@@ -1,3 +1,2 @@
 let baseUrl = 'https://katcloud.cn:8443/bili'
-// let baseUrl = 'https://192.168.31.88:8443/bili'
-// let baseUrl = 'https://192.168.1.18:8443/bili'
+// let baseUrl = 'https://192.168.31.188:8443/bili'
